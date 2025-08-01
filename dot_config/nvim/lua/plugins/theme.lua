91862d82485f23293f7c064061d413a03a288dec
Yousif Alatawi <github.paycheck@josephya.com>
2025-08-01 22:53:21 +0300

    Configured ghostty theme, setup defaults with hyprland, setup basic mako configs, setup nvim

---
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
