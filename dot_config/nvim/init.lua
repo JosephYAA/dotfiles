dd386520abe9ff5ce06157132a3e5dca0785860c
Yousif Alatawi <github.paycheck@josephya.com>
2025-10-18 10:23:46 +0300

    changed some stuff

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.api.nvim_create_autocmd("VimLeave", {
+  pattern = "*",
+  command = "set guicursor=a:ver25-blinkon500",
+})
